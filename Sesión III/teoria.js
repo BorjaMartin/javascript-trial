80cc3e55ea4be826b44e74cbef1653c1802ee677
Borja Martín <bmartinf@axpe.com>
2014-06-04 16:42:31 +0200

    Show JSON format

--- Sesión III/teoria.js
+++ Sesión III/teoria.js
@@ -1,31 +1,11 @@
-//funcion EVAL
-document.write('<H3>Función eval</H3>');
-eval("document.write('<li>evaluado!</li>');");
-eval("document.write(\"<li>comillas!</li>\");");
+//JSON
+document.write('<H3>JSON, Introducción</H3>');
+// Definimos una cadena en formato JSON
+array_cadena = '{"nombre":"Pepe","direccion":{"calle":"Victoria,6","num":7}}';
+document.write('<LI>' + array_cadena + '</LI>');
 
-// Parsers
-document.write('<H3>Parse</H3>');
-x = '111'
-a = parseInt(x);
-b = 222 ;
-y = parseFloat("4,50");
-document.write('<li>'+(a+b)+'</li>');
-document.write('<li>'+(x+b)+'</li>');
-document.write('<li>'+(y+10)+'</li>');
-document.write('<li>'+ isNaN(x)+'</li>');
-document.write('<li>'+(parseInt(1100,2))+' <- es 1100 en binario.</li>');
+var persona = JSON.parse(array_cadena)
+var name = persona.nombre
 
-
-// Objetos y funciones
-document.write('<H3>Objetos y Funciones</H3>');
-juego = {};
-juego.favorito = 'basket';
-var incoherente = ' gym is art     ';
-document.write('<li>'+ juego.favorito +'</li>');
-document.write('<li>'+ juego.favorito.charAt(3) +'</li>');
-document.write('<li>'+ juego.favorito.indexOf('e') +'</li>');
-document.write('<li>'+ juego.favorito.indexOf('o') +'</li>');
-document.write('<li>'+ juego.favorito.substring('1','3') +'</li>');
-document.write('<li> I LOVE '+ juego.favorito.toUpperCase() +'</li>');
-document.write('<li>'+ incoherente.trim() +'</li>');
-document.write('<li>'+ incoherente.length +'</li>');
+document.write('<LI> nombre: ' + name + '</LI>');
+document.write('<LI> calle: ' + persona.direccion.calle + '</LI>');
